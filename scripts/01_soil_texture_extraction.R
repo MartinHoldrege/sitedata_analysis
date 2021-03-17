0f5163874d0a97caedf08a9e97bb25f38a21d254
Martin Holdrege <martinholdrege@gmail.com>
2021-03-17 09:45:00 -0600

    old changes so can pull

--- scripts/01_soil_texture_extraction.R
+++ scripts/01_soil_texture_extraction.R
@@ -2,13 +2,8 @@
 
 # Script started 3/1/21
 
-# Purpose is to extract Conus-soil soil texture data for each site
+# Purpose is to extract soil texture data for each site
 
-
-# dependencies ------------------------------------------------------------
-
-library(tidyverse)
-library(soilDB) # for getting soil properties
 
 # load site locations -----------------------------------------------------
 
@@ -20,4 +15,10 @@ site1 <- readxl::read_xlsx("../dbWeather/200sites.xlsx")
 # just need coordinates
 site2 <- site1[ , c("site_id", "X_WGS84", "Y_WGS84")]
 
-sgrids1 <- fetchSoilGrids(site2[1:2, ], loc.names = c("site_id", "Y_WGS84", "X_WGS84"))
+
+# get soil data -----------------------------------------------------------
+
+# this takes maybe 20 minutes
+sgrids1 <- soilDB::fetchSoilGrids(site2, loc.names = c("site_id", "Y_WGS84", "X_WGS84"))
+
+saveRDS(sgrids1, "data-raw/soil_profiles_200sites.RDS")
