5fbbc90b1d5212d76eca746e843d4806a4cf0064
Martin Holdrege <martinholdrege@gmail.com>
2021-07-29 11:14:06 -0600

    map of MAP created

--- scripts/site_map.R
+++ scripts/site_map.R
@@ -13,10 +13,14 @@ library(sf)
 
 # read in data ------------------------------------------------------------
 
-# * site locations -----------------------------------------------------
+# * site locations --------------------------------------------------------
 
 # info on the 200 sites simulations will be run for
 site1 <- readxl::read_xlsx("../dbWeather/200sites.xlsx")
+
+# * MAP -------------------------------------------------------------------
+# mean annual precip from WORLCLIM
+m1 <- raster("data-raw/wc2.1_5m_bio/wc2.1_5m_bio_12.tif")
 
 # * aridity index ---------------------------------------------------------
 
@@ -28,6 +32,8 @@ r1 <- raster("data-raw/aridity_index/ai_et0/ai_et0.tif")
 aridity1 <- read_csv("data-processed/aridity_by_site.csv")%>%
   dplyr::select(-matches("_SD$"))
 
+aridity2 <- aridity1 %>%
+  right_join(site1, by = c("site" = "site_id"))
 
 # * cover data ------------------------------------------------------------
 
@@ -54,7 +60,10 @@ states <- tigris::states(cb = TRUE, class = "sf")
 class(states)
 states2 <- st_transform(states, crs)
 site3 <- SpatialPoints(site2[2:3], proj4string = crs)
-
+site5 <- SpatialPointsDataFrame(
+  coords = aridity2[c("X_WGS84", "Y_WGS84")],
+  data = aridity2[c("site", "aridity_index", "PRECIP_ppt_Mean")],
+  proj4string = crs)
 
 dat2 <- dat
 crs(dat2) <- crs
@@ -70,6 +79,8 @@ r3 <- mask(r3, states3) # crop to extent of states
 # making coarser so that easier
 r4 <- raster::aggregate(r3, fact = 2, fun = mean)
 
+m2 <- crop(m1, bbox_new) %>%
+  mask(states3)
 
 # calculate % sites -------------------------------------------------------
 # need to calculate % sites within soil texture grid cells
@@ -103,7 +114,7 @@ plot(aridity_index ~ aridity_gridded, data = site4,
      ylab = "aridity (STEPWAT2)")
 abline(0, 1)
 
-# map ---------------------------------------------------------------------
+# map gridded aridity--------------------------------------------------------
 
 colors <- RColorBrewer::brewer.pal(11, "RdYlBu")[c(3:7, 9)]
 
@@ -130,4 +141,51 @@ tm_shape(site3) +
 
 dev.off()
 
+# map aridity of points ---------------------------------------------------
+
+# just showing points (colored by aridity)
+jpeg("figures/maps/site_map_points-only.jpg",
+     res = 600, height = 4.5, width = 4.5, units = "in")
+tmap_mode("plot")
+
+tm_style(style = "white",
+         frame = "white",
+         legend.position = c("left", "bottom")
+         #legend.outside = TRUE
+         ) +
+  tm_shape(states3, bbox = bbox_new) +
+  tm_borders(col = "black") +
+  tm_shape(site5) +
+  tm_dots(col = "aridity_index",
+          breaks = c(0, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 10),
+          palette = RColorBrewer::brewer.pal(11, "RdYlBu")[c(2:7, 9)],
+          labels = c("< 0.2", "0.2 - 0.3", "0.3 - 0.4", "0.4 - 0.5", "0.5 - 0.6",
+                     "0.6 - 0.7", "> 07"),
+          title = "Aridity Index",
+          size = 0.2,
+          shape = 16)
+
+dev.off()
+
+# map gridded MAP -----------------------------------------------------------
+
+jpeg("figures/maps/site_map_MAP.jpg",
+     res = 600, height = 4.5, width = 4.5, units = "in")
+
+tm_style(style = "white",
+         frame = FALSE,
+         legend.position = c("left", "bottom"),
+         legend.bg.color = "white") +
+  tm_shape(m2) +
+  tm_raster(breaks = c(0,200, 300, 400, 500, 600, 700, 800, 10000),
+            labels = c("< 200", "200 - 300", "300 - 400", "400 - 500",
+                       "500 - 600", "600 - 700", "700 - 800", "> 800"),
+            palette = RColorBrewer::brewer.pal(11, "RdYlBu")[2:9],
+            title = "MAP (mm)")+
+  tm_shape(states3, bbox = bbox_new) +
+  tm_borders(col = "black") +
+  tm_shape(site3) +
+  tm_dots(size = 0.1)
+
+dev.off()
 
